953d9fc27a8e5cccc1e52d6ee0e70e08bfc306fd
Borja <borjafer06@gmail.com>
2026-03-12 19:20:08 +0100

    Add files via upload

---
+++ Ejercicio2Bien/Ejercicio2Bien/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicio2Bien
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Servidor()).Init();
+        }
+    }
+}
---
+++ Ejercicio2Bien/Ejercicio2Bien/Usuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio2Bien
+{
+    internal class Usuario
+    {
+        private string nombre;
+        private string ip;
+        private StreamWriter streamWriter;
+
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public string getNombre()
+        {
+            return nombre;
+        }
+
+        public void setIp(string ip)
+        {
+            this.ip = ip;
+        }
+        public string getIp()
+        {
+            return ip;
+        }
+
+        public void setStreamWriter(StreamWriter streamWriter)
+        {
+            this.streamWriter = streamWriter;
+        }
+        public StreamWriter getStreamWriter()
+        {
+            return streamWriter;
+        }
+    }
+}
