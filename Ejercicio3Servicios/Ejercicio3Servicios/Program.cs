bcaf4e2718bb2e27882130b4380b1bcfb598bd81
Borja <borjafer06@gmail.com>
2026-03-12 19:18:53 +0100

    Add files via upload

---
+++ Ejercicio3Servicios/Ejercicio3Servicios/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio3Servicios
+{
+    internal class Class1
+    {
+        static void Main(String[] args)
+        {
+            (new ShiftServer()).Init();
+        }
+    }
+}
