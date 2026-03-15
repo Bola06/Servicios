c69d66b07d1b9b87c204fcf183ea4af80d185aec
Borja <borjafer06@gmail.com>
2026-03-15 18:19:46 +0100

    Add files via upload

---
+++ Ej1Servidor/Ej1Servidor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Ej1Servidor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         (new Ej1Servidor()).InitServer();
+            
+        }
+    }
+}
